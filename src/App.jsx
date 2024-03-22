1e810af3f711c381314da015aab0737e9bfaf5cb
Bruno Alpachino <brunolalpachino@gmail.com>
2024-03-22 14:16:40 +0300

    styling of components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Bruno</span>
+            <span className="player-symbol">x</span>
+          </li>
+          <li>
+            <span className="player-name">Maxmillian</span>
+            <span className="player-symbol">x</span>
+          </li>{" "}
+        </ol>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
