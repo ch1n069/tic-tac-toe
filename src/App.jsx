9b380c2b19b137e5cb1a48719a7b1ae78d12bd28
Bruno Alpachino <brunolalpachino@gmail.com>
2024-04-04 01:42:17 +0300

    feat ===  created the gameboard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { useState } from "react";
 import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
 
 function App() {
+  const [playerName, setPlayerName] = useState("Player");
+  // handle state update by passing props up
+  const updatePlayerNameHandler = () => {};
+
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <Player name="Player 1" symbol={"x"} />
+          <Player name={playerName} symbol={"x"} />
           <Player name="Player 2" symbol={"x"} />
         </ol>
+        <GameBoard />
       </div>
     </main>
   );
