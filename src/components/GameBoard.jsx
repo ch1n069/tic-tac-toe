ecdb0a36608d9f6191c88601f934c55db8fb8da7
Bruno Alpachino <brunolalpachino@gmail.com>
2024-05-19 08:10:30 +0300

    bug(fix) === fixed bug not rendering gameboard

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const initialGameBoard = [
   [null, null, null],
@@ -6,19 +6,32 @@ const initialGameBoard = [
   [null, null, null],
 ];
 const GameBoard = () => {
+  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  // update game board handler
+  // const handleSelectSquare = (rowIndex, colIndex) => {
+  //   console.log("I was clicked");
+  //   setGameBoard((prevGameBoard) => {
+  //     const updatedBoard = [
+  //       ...prevGameBoard.map((innerArray) => [...innerArray]),
+  //     ];
+  //     updatedBoard[rowIndex][colIndex] = "X";
+  //     return updatedBoard;
+  //   });
+  // };
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => {
+      {initialGameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
-            {row.map((playerSymbol, colIndex) => {
+            {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
-              </li>;
-            })}
-          </ol>
-        </li>;
-      })}
+                <button type="">{playerSymbol}</button>
+              </li>
+            ))}
+          </ol>{" "}
+        </li>
+      ))}
     </ol>
   );
 };
